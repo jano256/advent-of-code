03dc6ffce305b7f7417e6e6b090653fef4e5f7b4
jano1312 <janomail@gmx.de>
2024-12-15 23:13:03 +0100

    day 11b

--- year/2024/day11/b.py
+++ year/2024/day11/b.py
@@ -1,30 +1,24 @@
-import math
-from timeit import timeit
+from functools import cache
 
 def main():
     with open("input.txt", "r") as f:
         stones = list(map(int, f.read().split()))
 
     stone_count = 0
-    time = timeit("count(17, 35)", globals=globals(), number=1)
-    print(time)
-
     for stone in stones:
-        stone_count += count(stone, 25)
+        stone_count += count(stone, 75)
     print(stone_count)
 
+@cache
 def count(stone, round):
     if round == 0:
         return 1
     
     if stone == 0:
         return count(1, round - 1)
-    
     digits = len(str(stone))
     if digits % 2 == 0:
         return count(int(str(stone)[digits//2:]), round - 1) + count(int(str(stone)[:digits//2]), round - 1)
-    else:
-        return count(stone * 2024, round - 1)
-
+    return count(stone * 2024, round - 1)
 
 main()
