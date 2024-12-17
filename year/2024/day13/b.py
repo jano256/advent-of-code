eb310f3bc9b8992fca23487f801b83f234c3dc38
jano256 <janomail@gmx.de>
2024-12-17 01:48:30 +0100

    day 13 b

--- year/2024/day13/b.py
+++ year/2024/day13/b.py
@@ -2,7 +2,7 @@ import re
 
 def main():
     machines = []
-    with open("test.txt", "r") as f:
+    with open("input.txt", "r") as f:
         for machine in f.read().split("\n\n"):
             machines.append(map(int, re.findall(r'\d+', machine)))
 
@@ -10,26 +10,13 @@ def main():
     for ax, ay, bx, by, x, y in machines:
         x += 10000000000000
         y += 10000000000000
-        win = False
-        cost = 99999999
-        for a in range(100):
-            for b in range(100):
-                cx = a * ax + b * bx
-                cy = a * ay + b * by
-                if cx == x and cy == y:
-                    win = True
-                    new_cost = 3 * a + b
-                    if new_cost < cost:
-                        cost = new_cost
-        if win:
-            total_cost += cost
-    
+
+        a = (x * by - y * bx) / (by * ax - bx * ay)
+        b = (x * ay - y * ax) / (ay * bx - by * ax)
+        
+        if a % 1 == 0 and b % 1 == 0:
+            total_cost += round(3 * a + b)
+        
     print(total_cost)
 
 main()
-
-'''
-zwei linien, die sich treffen
-a von 0/0 richung ziel, b von ziel richtung 0/0
-wenn die punkte der linien sich treffen, ist es das ergebnis
-'''
